6ae467f7d0915ca5da2a79f27020d05913f8210e
KimHaNeul <s2025@e-mirim.hs.kr>
2021-08-18 23:09:00 +0900

    add animation effect

--- app/src/main/java/com/example/energize/MainActivity.java
+++ app/src/main/java/com/example/energize/MainActivity.java
@@ -5,17 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    //bottom page animation
+    RelativeLayout bottomPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //remove splash theme
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
+        //bottom page animation
+        final Animation translateup = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_up);
+        bottomPage = findViewById(R.id.bottom_page);
 
-
+        bottomPage.setVisibility(View.VISIBLE);
+        bottomPage.startAnimation(translateup);
 
 
     }
