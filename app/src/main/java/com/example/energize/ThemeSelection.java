49ab4dcaef6414f2924f5b6f219eca1f7dba47c4
KimHaNeul <s2025@e-mirim.hs.kr>
2021-08-20 02:18:59 +0900

    create each theme_layout

--- app/src/main/java/com/example/energize/ThemeSelection.java
+++ app/src/main/java/com/example/energize/ThemeSelection.java
@@ -2,16 +2,23 @@ package com.example.energize;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class ThemeSelection extends AppCompatActivity {
 
     //bottom page animation
     RelativeLayout bottomPage;
+    //each theme button
+    Button theme_solar;
+    Button theme_hydro;
+    Button theme_wind;
+    Button theme_geo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,41 @@ public class ThemeSelection extends AppCompatActivity {
         bottomPage.setVisibility(View.VISIBLE);
         bottomPage.startAnimation(translateup);
 
+
+        theme_solar = findViewById(R.id.Btn_solar);
+        theme_hydro = findViewById(R.id.Btn_hydroelectric);
+        theme_wind = findViewById(R.id.Btn_wind);
+        theme_geo = findViewById(R.id.Btn_geothermal);
+
+        //move to each theme activity(fragment)
+        theme_solar.setOnClickListener(v -> {
+            Intent intent = new Intent(this,Theme_Solar.class);
+            startActivity(intent);
+            //starting activity animation
+            overridePendingTransition(R.anim.translate_none,R.anim.translate_right);
+            finish();
+        });
+        theme_hydro.setOnClickListener(v -> {
+            Intent intent = new Intent(this,Theme_Hydro.class);
+            startActivity(intent);
+            //starting activity animation
+            overridePendingTransition(R.anim.translate_none,R.anim.translate_right);
+            finish();
+        });
+        theme_wind.setOnClickListener(v -> {
+            Intent intent = new Intent(this,Theme_Wind.class);
+            startActivity(intent);
+            //starting activity animation
+            overridePendingTransition(R.anim.translate_none,R.anim.translate_right);
+            finish();
+        });
+        theme_geo.setOnClickListener(v -> {
+            Intent intent = new Intent(this,Theme_Geo.class);
+            startActivity(intent);
+            //starting activity animation
+            overridePendingTransition(R.anim.translate_none,R.anim.translate_right);
+            finish();
+        });
 
         //            overridePendingTransition(R.anim.translate_none,R.anim.translate_right);
         //            finish();
