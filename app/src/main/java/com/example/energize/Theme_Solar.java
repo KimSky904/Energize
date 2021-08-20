d2b8748277bc4858b8baedaa5d9d8e7793d1e9aa
KimHaNeul <s2025@e-mirim.hs.kr>
2021-08-21 04:25:38 +0900

    add quiz solving steps (not completed)

--- app/src/main/java/com/example/energize/Theme_Solar.java
+++ app/src/main/java/com/example/energize/Theme_Solar.java
@@ -3,41 +3,224 @@ package com.example.energize;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class Theme_Solar extends AppCompatActivity {
 
-    ImageView next_q2;
+    //previous / next button
+    ImageView previous;
+    ImageView next;
+    //question
     RelativeLayout question1;
     RelativeLayout question2;
+    RelativeLayout question3;
+    RelativeLayout question4;
+    RelativeLayout question5;
+    RelativeLayout question6;
+    RelativeLayout question7;
+    RelativeLayout question8;
+    RelativeLayout question9;
+    RelativeLayout question10;
+    //each answer button
+    Button question1_a1,question1_a2,question1_a3,question1_a4;
+    Button question2_a1,question2_a2,question2_a3,question2_a4;
+    Button question3_a1,question3_a2,question3_a3,question3_a4;
+    Button question4_a1,question4_a2,question4_a3,question4_a4;
+    Button question5_a1,question5_a2,question5_a3,question5_a4;
+    Button question6_a1,question6_a2,question6_a3,question6_a4;
+    Button question7_a1,question7_a2,question7_a3,question7_a4;
+    Button question8_a1,question8_a2,question8_a3,question8_a4;
+    Button question9_a1,question9_a2,question9_a3,question9_a4;
+    Button question10_a1,question10_a2,question10_a3,question10_a4;
+    //index Key
+    int index = 0;
+    //question 1~10 array
+    View[] question;
+    //animation
+    Animation tranlateLeftAnim;
+    Animation tranlateRightAnim;
+    //save the currently displayed screen
+    View NowDisplayScreen; //1 ~ 10;
+    //save the pre/next screen
+    View PreDisplayScreen;
+    View NextDisplayScreen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme_solar);
 
-
-
-        //bottom page animation
+        //bottom page animation (only first page)
         final Animation translateup = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_up);
         question1 = findViewById(R.id.question1);
         question1.setVisibility(View.VISIBLE);
         question1.startAnimation(translateup);
 
-        next_q2 = findViewById(R.id.next_q2);
+        //previous / next button
+        previous = findViewById(R.id.previous_btn);
+        next = findViewById(R.id.next_btn);
+
+        //question & view
+        question1_a1 = findViewById(R.id.question1_a1);
+        question1_a2 = findViewById(R.id.question1_a2);
+        question1_a3 = findViewById(R.id.question1_a3);
+        question1_a4 = findViewById(R.id.question1_a4);
+
         question2 = findViewById(R.id.question2);
-        next_q2.setOnClickListener(v -> {
-            question1.setVisibility(View.INVISIBLE);
-            question2.setVisibility(View.VISIBLE);
-            //starting activity animation
-            overridePendingTransition(R.anim.translate_none,R.anim.translate_right);
-            finish();
+        question2_a1 = findViewById(R.id.question2_a1);
+        question2_a2 = findViewById(R.id.question2_a2);
+        question2_a3 = findViewById(R.id.question2_a3);
+        question2_a4 = findViewById(R.id.question2_a4);
+
+        question3 = findViewById(R.id.question3);
+        question3_a1 = findViewById(R.id.question3_a1);
+        question3_a2 = findViewById(R.id.question3_a2);
+        question3_a3 = findViewById(R.id.question3_a3);
+        question3_a4 = findViewById(R.id.question3_a4);
+
+        question4 = findViewById(R.id.question4);
+        question4_a1 = findViewById(R.id.question4_a1);
+        question4_a2 = findViewById(R.id.question4_a2);
+        question4_a3 = findViewById(R.id.question4_a3);
+        question4_a4 = findViewById(R.id.question4_a4);
+
+        question5 = findViewById(R.id.question5);
+        question5_a1 = findViewById(R.id.question5_a1);
+        question5_a2 = findViewById(R.id.question5_a2);
+        question5_a3 = findViewById(R.id.question5_a3);
+        question5_a4 = findViewById(R.id.question5_a4);
+
+        question6 = findViewById(R.id.question6);
+        question6_a1 = findViewById(R.id.question6_a1);
+        question6_a2 = findViewById(R.id.question6_a2);
+        question6_a3 = findViewById(R.id.question6_a3);
+        question6_a4 = findViewById(R.id.question6_a4);
+
+        question7 = findViewById(R.id.question7);
+        question7_a1 = findViewById(R.id.question7_a1);
+        question7_a2 = findViewById(R.id.question7_a2);
+        question7_a3 = findViewById(R.id.question7_a3);
+        question7_a4 = findViewById(R.id.question7_a4);
+
+        question8 = findViewById(R.id.question8);
+        question8_a1 = findViewById(R.id.question8_a1);
+        question8_a2 = findViewById(R.id.question8_a2);
+        question8_a3 = findViewById(R.id.question8_a3);
+        question8_a4 = findViewById(R.id.question8_a4);
+
+        question9 = findViewById(R.id.question9);
+        question9_a1 = findViewById(R.id.question9_a1);
+        question9_a2 = findViewById(R.id.question9_a2);
+        question9_a3 = findViewById(R.id.question9_a3);
+        question9_a4 = findViewById(R.id.question9_a4);
+
+        question10 = findViewById(R.id.question10);
+        question10_a1 = findViewById(R.id.question10_a1);
+        question10_a2 = findViewById(R.id.question10_a2);
+        question10_a3 = findViewById(R.id.question10_a3);
+        question10_a4 = findViewById(R.id.question10_a4);
+
+
+
+        //save each page as int (like key)
+        question = new View[]{question1, question2, question3, question4, question5, question6, question7, question8, question9, question10};
+        //save the currently displayed screen
+        View NowDisplayScreen = question[0]; //1 ~ 10;
+        //save the pre/next screen
+        View PreDisplayScreen = question[0];
+        View NextDisplayScreen = question[1];
+        if(index==0) previous.setVisibility(View.INVISIBLE);
+
+        //animation in anim directory
+        tranlateLeftAnim = AnimationUtils.loadAnimation(this,R.anim.translate_left);
+        tranlateRightAnim = AnimationUtils.loadAnimation(this,R.anim.translate_right);
+        // sence page sliding event
+        SlidingPageAnimationListener animListener = new SlidingPageAnimationListener();
+        tranlateLeftAnim.setAnimationListener(animListener);
+        tranlateRightAnim.setAnimationListener(animListener);
+
+
+
+        next.setOnClickListener(v -> {
+            NowDisplayScreen.setVisibility(View.INVISIBLE);
+            NextDisplayScreen.setVisibility(View.VISIBLE);
+            Log.d("myapp",NextDisplayScreen.toString());
+            index++;
+            if(index>10) index = 10;
+            Log.d("myapp","index : "+index+" ");
+            NextDisplayScreen.startAnimation(tranlateRightAnim);
+            NowDisplayScreen.startAnimation(tranlateLeftAnim);
+
+        });
+        previous.setOnClickListener(v -> {
+            NowDisplayScreen.setVisibility(View.INVISIBLE);
+            PreDisplayScreen.setVisibility(View.VISIBLE);
+            Log.d("myapp",NextDisplayScreen.toString());
+            index--;
+            if(index<0) index = 0;
+            NowDisplayScreen.startAnimation(tranlateLeftAnim);
+            PreDisplayScreen.startAnimation(tranlateRightAnim);
+            Log.d("myapp","index : "+index+" ");
         });
 
 
+        //next btn -> move to next page
+        /*
+        next.setOnClickListener(v -> {
+            NowDisplayScreen.setVisibility(View.INVISIBLE);
+            NextDisplayScreen.setVisibility(View.VISIBLE);
+            Log.d("myapp",NextDisplayScreen.toString());
+            addIndex(index);
+            Log.d("myapp","index : "+index+" ");
+            //starting activity animation
+            //overridePendingTransition(R.anim.translate_none,R.anim.translate_right);
+        }); */
+        //previous btn -> move to previous page
+        /*
+        previous.setOnClickListener(v -> {
+            NowDisplayScreen.setVisibility(View.INVISIBLE);
+            PreDisplayScreen.setVisibility(View.VISIBLE);
+            Log.d("myapp",PreDisplayScreen.toString());
+            //starting activity animation
+            //overridePendingTransition(R.anim.translate_none,R.anim.translate_right);
+        });
+        */
+    }
+
+    private class SlidingPageAnimationListener implements Animation.AnimationListener{
+        @Override
+        public void onAnimationStart(Animation animation) { }
+        public void onAnimationEnd(Animation animation){
+            Log.d("myapp","애니메이션 발생함");
+            //remove previous button if question 1
+            if(index==0) previous.setVisibility(View.INVISIBLE);
+            else previous.setVisibility(View.VISIBLE);
+
+            //hide irrelevant question page
+            for(int i=0;i<10;i++){
+                if(i==index||i==(index-1)||i==(index+1)) continue;
+                question[i].setVisibility(View.INVISIBLE);
+                Log.i("visiblity",i+"번째 문제 : "+question[i].getVisibility()+" ");
+            }
+
+            //set each screen status
+            //now
+            NowDisplayScreen = question[index];
+            //prev
+            if(index==0) PreDisplayScreen = question[0];
+            else PreDisplayScreen = question[index-1];
+            //next
+            if(index==9) NextDisplayScreen = question[9];
+            else NextDisplayScreen = question[index+1];
+        }
+        @Override
+        public void onAnimationRepeat(Animation animation) { }
     }
 }
