fc12580992665a50f8544eaf5395fe6ee68ffeb3
KimHaNeul <s2025@e-mirim.hs.kr>
2021-08-25 02:00:20 +0900

    account detail 종료 후 값 유지

--- app/src/main/java/com/example/energize/ResultScreen.java
+++ app/src/main/java/com/example/energize/ResultScreen.java
@@ -1,6 +1,7 @@
 package com.example.energize;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -16,17 +17,28 @@ public class ResultScreen extends AppCompatActivity {
     Button go_theme;
     //text of acquired point
     Button btn_chooseAvatar;
-
+    //값 유지
+    String shared = "file";
+    int saving;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_screen);
-
-        //get each points
+        //원형 버튼 텍스트
         btn_chooseAvatar = findViewById(R.id.btn_chooseAvatar);
-        btn_chooseAvatar.setText(User.point.getEachPoint()+" out 20 points");
-        User.point.initialThemePoint();
-
+        saving = User.point.getEachPoint();
+        //획득 포인트 저장
+        if(User.point.getEachPoint()==0) {
+            //account detail 페이지 갔다온 후
+            SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+            int pointted = sharedPreferences.getInt("point", 0);
+            btn_chooseAvatar.setText(pointted+" out 20 points");
+        }
+        else {
+            //get each points
+            btn_chooseAvatar.setText(User.point.getEachPoint() + " out 20 points");
+            User.point.initialThemePoint();
+        }
 
         //move to account details
         go_avatar = findViewById(R.id.go_avatar);
@@ -45,5 +57,18 @@ public class ResultScreen extends AppCompatActivity {
             Intent intent = new Intent(this,ThemeSelection.class);
             startActivity(intent);
         });
+
+
+    }
+
+    //detail 페이지로 갔다온 후에도 얻은 포인트 값 유지
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(shared,0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("point",saving);
+        editor.commit();
     }
 }
