dc125de52d4f6a6a20bd35b035a54270e142bf4d
KimHaNeul <s2025@e-mirim.hs.kr>
2021-08-26 23:17:37 +0900

    아바타 구매 작업 완료

--- app/src/main/java/com/example/energize/SelectAvatar.java
+++ app/src/main/java/com/example/energize/SelectAvatar.java
@@ -2,15 +2,22 @@ package com.example.energize;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class SelectAvatar extends AppCompatActivity {
 
@@ -20,6 +27,12 @@ public class SelectAvatar extends AppCompatActivity {
     ImageButton Btn_back;
     //point text
     Button btn_point;
+
+    //구매된 아바타인지, 미구매 아바타인지 구분
+    int[] avatarIsAvailable = {1,1,1,1,0,0,0,0};
+    //이미지파일 배열
+    int[] avatar = {R.drawable.img_avatar_1,R.drawable.img_avatar_2,R.drawable.img_avatar_3,R.drawable.img_avatar_4,R.drawable.img_avatar_5,
+            R.drawable.img_avatar_6,R.drawable.img_avatar_7,R.drawable.img_avatar_8};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,68 +54,137 @@ public class SelectAvatar extends AppCompatActivity {
 
         Btn_back = findViewById(R.id.btn_back);
         Btn_back.setOnClickListener(v -> {
+            Log.d("myapp",User.point.getAvatar_image()+"가 선택됨");
             Intent intent = new Intent(this,AccountDetails.class);
             startActivity(intent);
         });
 
         //image resource return
+        /*
         avatar_1.setOnClickListener(v -> {
-            //intent
-            Intent intent = new Intent(this,AccountDetails.class);
-            intent.putExtra("selected_avatar",R.drawable.img_avatar_1);
-            setResult(RESULT_OK, intent);
-            startActivity(intent);
+            ViewDialog alert = new ViewDialog();
+            alert.showDialog(SelectAvatar.this);
         });
         avatar_2.setOnClickListener(v -> {
-            //intent
-            Intent intent = new Intent(this,AccountDetails.class);
-            intent.putExtra("selected_avatar",R.drawable.img_avatar_2);
-            setResult(RESULT_OK, intent);
-            startActivity(intent);
+
         });
         avatar_3.setOnClickListener(v -> {
-            //intent
-            Intent intent = new Intent(this,AccountDetails.class);
-            intent.putExtra("selected_avatar",R.drawable.img_avatar_3);
-            setResult(RESULT_OK, intent);
-            startActivity(intent);
+
         });
         avatar_4.setOnClickListener(v -> {
-            //intent
-            Intent intent = new Intent(this,AccountDetails.class);
-            intent.putExtra("selected_avatar",R.drawable.img_avatar_4);
-            setResult(RESULT_OK, intent);
-            startActivity(intent);
+
         });
         avatar_5.setOnClickListener(v -> {
-            //intent
-            Intent intent = new Intent(this,AccountDetails.class);
-            intent.putExtra("selected_avatar",R.drawable.img_avatar_5);
-            setResult(RESULT_OK, intent);
-            startActivity(intent);
+
         });
         avatar_6.setOnClickListener(v -> {
-            //intent
-            Intent intent = new Intent(this,AccountDetails.class);
-            intent.putExtra("selected_avatar",R.drawable.img_avatar_6);
-            setResult(RESULT_OK, intent);
-            startActivity(intent);
+
         });
         avatar_7.setOnClickListener(v -> {
-            //intent
-            Intent intent = new Intent(this,AccountDetails.class);
-            intent.putExtra("selected_avatar",R.drawable.img_avatar_7);
-            setResult(RESULT_OK, intent);
-            startActivity(intent);
+
         });
         avatar_8.setOnClickListener(v -> {
-            //intent
-            Intent intent = new Intent(this,AccountDetails.class);
-            intent.putExtra("selected_avatar",R.drawable.img_avatar_8);
-            setResult(RESULT_OK, intent);
-            startActivity(intent);
+
         });
 
+         */
 
+
+    }
+
+    public void buttonClicked(View view){
+        int avatarId = 1;
+        switch (view.getId()){
+            case R.id.avatar_1:
+                avatarId = 1;
+                break;
+            case R.id.avatar_2:
+                avatarId = 2;
+                break;
+            case R.id.avatar_3:
+                avatarId = 3;
+                break;
+            case R.id.avatar_4:
+                avatarId = 4;
+                break;
+            case R.id.avatar_5:
+                avatarId = 5;
+                break;
+            case R.id.avatar_6:
+                avatarId = 6;
+                break;
+            case R.id.avatar_7:
+                avatarId = 7;
+                break;
+            case R.id.avatar_8:
+                avatarId = 8;
+                break;
+            default: avatarId=1;
+        }
+        //배열값이 1으로 되어있는 경우 avatar set
+        if(avatarIsAvailable[avatarId]==1){
+            User.point.setAvatar_image(avatar[avatarId]);
+            Log.d("myapp","아바타가 "+User.point.getAvatar_image()+"로 변경됨");
+        }
+
+        //배열값이 0으로 되어있는 경우 구매확인 팝업 띄움 & 배열값 1로 변경
+        else if(avatarIsAvailable[avatarId]==0){
+            avatarIsAvailable[avatarId]=1;
+            ViewDialog alert = new ViewDialog();
+            alert.showDialog(SelectAvatar.this,avatarId);
+        }
+
+    }
+
+
+    public class ViewDialog {
+
+        public void showDialog(Activity activity,int avatarId) {
+            final Dialog dialog = new Dialog(activity);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(false);
+            dialog.setContentView(R.layout.activity_avatar_purchase_confirm_dialog);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+/*
+            int avatarRes;
+            switch (avatarId){
+                case 1:
+                    avatarRes = R.drawable.img_avatar_1;
+                    break;
+                case 2:
+                    avatarRes = R.drawable.img_avatar_2;
+                    break;
+                case 3:
+                    avatarRes = R.drawable.img_avatar_3;
+                    break;
+                case 4:
+                    avatarRes = R.drawable.img_avatar_4;
+                    break;
+                case 5:
+                    avatarRes = R.drawable.img_avatar_5;
+                    break;
+                case 6:
+                    avatarRes = R.drawable.img_avatar_6;
+                    break;
+                case 7:
+                    avatarRes = R.drawable.img_avatar_7;
+                    break;
+                case 8:
+                    avatarRes = R.drawable.img_avatar_8;
+                    break;
+                default:
+                    avatarRes = R.drawable.img_avatar_1;
+            }
+
+ */
+            FrameLayout mDialogOk = dialog.findViewById(R.id.frmOk);
+            mDialogOk.setOnClickListener(v ->  {
+                Toast.makeText(getApplicationContext(),"Okay" ,Toast.LENGTH_SHORT).show();
+                dialog.cancel();
+            });
+
+            dialog.show();
+        }
     }
 }
